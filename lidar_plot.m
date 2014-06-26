45bde380c4a8bb63df41fd3835505beebe78c143
BradleyClymer <brad.clymer@gmail.com>
2014-06-26 17:27:20 -0400

    Add dummy anonymous function

--- lidar_plot.m
+++ lidar_plot.m
@@ -1,12 +1,12 @@
 clc
 close all hidden 
-f               = figure( 'NumberTitle' , 'Off' , 'Name' , 'Lidar Angle Illustration' )
+f               = figure( 'NumberTitle' , 'Off' , 'Name' , 'Lidar Angle Illustration' , 'Units' , 'Normalized' , 'Position' , [ 0.55 0.2 0.4 0.4 ] )
 axes
 hold on
 vec_angle       = @( a , b ) acosd( dot( a , b ) / ( norm( a ) * norm( b ) ) )  ;
 source.pos      = [ 1 0 ]                                                       ;
 target.pos      = [ -5 0 ]                                                      ;
-target.angle    = 90 + 35/2                                                     ;
+target.angle    = 90 + 25/2                                                     ;
 target.vec      = [ cosd( target.angle ) sind( target.angle ) ]                 ;
 target.slope    = target.vec( 2 ) / target.vec( 1 )                             ;
 target.theta    = atand( target.slope )                            
@@ -21,15 +21,19 @@ h.out           = plot( [ source.pos( 1 ) target.pos( 1 ) ] , [ source.pos( 2 ) 
 
 ret.angle       = 180 + 2 * target.theta                                              
 ret.vec         = [ cosd( ret.angle ) sind( ret.angle ) ]                       ;
-h.ret           = plot( 10*[ 0 ret.vec( 1 ) ] + target.pos( 1 ) ,                    ...
-                        10*[ 0 ret.vec( 2 ) ] + target.pos( 2 ) ,                    ...
+ret.position    = [ 10*[ 0 ret.vec( 1 ) ] + target.pos( 1 ) ;                   ...
+                    10*[ 0 ret.vec( 2 ) ] + target.pos( 2 ) ]                   
+h.ret           = plot( 10*[ 0 ret.vec( 1 ) ] + target.pos( 1 ) ,            	...
+                        10*[ 0 ret.vec( 2 ) ] + target.pos( 2 ) ,            	...
                         'LineSmoothing' , 'on' , 'Color' , [ 1 0 0 ]  )
-
+target.rot      = @( dummy ) 3^2 
+target.rot( target )
 % scratch.angle   = acosd( dot( ret.vec , scratch.vec ) / ( norm( ret.vec , scratch.vec ) )
 scratch.theta   = vec_angle( scratch.vec , ret.vec )
-
+dot( ret.vec , target.vec )
 
 grid on
-axis equal
+
 xlim( [ -6 1 ] )
+axis equal
 % ylim( [ -1 2 ] )
